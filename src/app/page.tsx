d604be4b763dc6374cd2e29999fc6a05b9dd8ac0
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-14 20:25:00 +0800

    savepoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Home
+    <div className='flex justify-center mt-20'>
+      <button className='text-center font-bold bg-gray-700 w-32 justify-center flex rounded-md text-nowrap py-1 curosr-pointer hover:bg-gray-600'><Link href="/musics">Get Started</Link></button>
     </div>
   )
 }
